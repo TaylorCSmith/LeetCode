ffcdad78f7a63d97f9878e0b6c935190f72932ae
Taylor Smith <tcs140230@utdallas.edu>
2017-06-28 16:50:50 -0500

    initial commit

---
+++ 561ArrayPartition1/ArrayPairSum.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class ArrayPairSum
+{
+    public void ArrayPairSumFunction(int[] nums)
+    {
+        // runs correctly
+        int sum = 0;
+
+        for (int n = 0; n < nums.Length; n = n + 2)
+        {
+            int[] temp = new int[2];
+            temp[0] = nums[n];
+            temp[1] = nums[n + 1];
+
+            sum = sum + temp.Min();
+        }
+
+        Console.WriteLine(sum);
+    }
+}
---
+++ 561ArrayPartition1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _561ArrayPartition1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayPairSum problem1 = new ArrayPairSum();
+
+            int[] nums = { 2, 1, 4, 5, 1, 4, 1, 3, 5, 2};
+
+            problem1.ArrayPairSumFunction(nums); // works but need to add exception if array has an odd number of elements
+
+
+
+        }
+    }
+}
